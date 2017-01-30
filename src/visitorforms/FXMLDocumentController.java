9d8e416a53da999c4e9b84f819c70c07ae4815c0
Shiv Shah <shah.shivbhushan@gmail.com>
2017-01-30 15:30:57 -0600

    Create and Bind User Interface

--- src/visitorforms/FXMLDocumentController.java
+++ src/visitorforms/FXMLDocumentController.java
@@ -10,7 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,10 +22,28 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private TextField txtFname;
+    @FXML
+    private TextField txtLname;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtPhone;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TableView<?> tblView;
+    @FXML
+    private TableColumn<?, ?> colFname;
+    @FXML
+    private TableColumn<?, ?> colLname;
+    @FXML
+    private TableColumn<?, ?> colEmail;
+    @FXML
+    private TableColumn<?, ?> colPhone;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +53,9 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void populateTable(ActionEvent event) {
+    }
     
 }
