c0c0d1a76c2eafb9bb521da6db83a69e875cae2f
Shiv Shah <shah.shivbhushan@gmail.com>
2017-01-30 18:11:15 -0600

    Update code in VisitorsDetails.java

--- src/visitorforms/VisitorsDetails.java
+++ src/visitorforms/VisitorsDetails.java
@@ -3,6 +3,7 @@
  */
 package visitorforms;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
@@ -10,49 +11,65 @@ import javafx.beans.property.StringProperty;
  * @author SBS
  */
 public class VisitorsDetails {
-    private String firstname;
-    private String lastname;
-    private String email;
-    private int phone;
+    private StringProperty firstname;
+    private StringProperty lastname;
+    private StringProperty email;
+    private StringProperty phone;
     
-    public VisitorsDetails(String fname, String lname, String email, int phone){
-        this.firstname = fname;
-        this.lastname = lname;
-        this.email = email;
-        this.phone = phone;
+    public VisitorsDetails(String fname, String lname, String email, String phone){
+        this.firstname = new SimpleStringProperty(fname);
+        this.lastname = new SimpleStringProperty(lname);
+        this.email = new SimpleStringProperty(email);
+        this.phone = new SimpleStringProperty(phone);
     }
     
     //getters
     public String getFname(){
-        return firstname;
+        return firstname.get();
     }
     
     public String getLname(){
-        return lastname;
+        return lastname.get();
     }
     
     public String getEmail(){
-        return email;
+        return email.get();
     }
     
-    public int getPhone(){
-        return phone;
+    public String getPhone(){
+        return phone.get();
     }
     
     //setters
     public void setFname(String fname){
-        firstname = fname;
+        firstname.set(fname);
     }
       
     public void setLname(String lname){
-        lastname = lname;
+        lastname.set(lname);
     }
     
     public void setEmail(String email){
-        this.email = email;
+        this.email.set(email);
     }
 
-    public void setPhone(int phone){
-        this.phone = phone;
+    public void setPhone(String phone){
+        this.phone.set(phone);
+    }
+    
+    public StringProperty fnameProperty(){
+        return firstname;
+    }
+    
+    public StringProperty lnameProperty(){
+        return lastname;
+    }
+    
+    public StringProperty emailProperty(){
+        return email;
+    }
+    
+    public StringProperty phoneProperty(){
+        return phone;
     }
 }
