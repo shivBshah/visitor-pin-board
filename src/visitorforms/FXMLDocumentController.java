001bb3f6534b097d5bb506d188269572a4a1dfb0
Shiv Shah <shah.shivbhushan@gmail.com>
2017-01-30 19:32:21 -0600

    Add method to save form data in database

--- src/visitorforms/FXMLDocumentController.java
+++ src/visitorforms/FXMLDocumentController.java
@@ -7,6 +7,7 @@ package visitorforms;
 
 import com.mysql.jdbc.Connection;
 import java.net.URL;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -62,11 +63,51 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         dcon = new DatabaseConnection();
+        displayFromDatabase();
     }    
 
     @FXML
     private void populateTable(ActionEvent event) {
+        saveToDatabase();
+        clearForm();
         displayFromDatabase();
+    }
+    
+    private void clearForm(){
+        txtFname.setText("");
+        txtLname.setText("");
+        txtEmail.setText("");
+        txtPhone.setText("");
+    }
+    
+    private void saveToDatabase(){
+        String fname = txtFname.getText();
+        String lname = txtLname.getText();
+        String email = txtEmail.getText();
+        String phone = txtPhone.getText();
+        
+        try {
+            Connection conn = dcon.connect();
+
+            //execute query and store data in database
+            String query = "INSERT INTO visitor(firstname,lastname,email,phone) VALUES(?,?,?,?)";
+            ResultSet rs = conn.createStatement().executeQuery(query);
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setString(1, fname);
+            pst.setString(2, lname);
+            pst.setString(3, email);
+            pst.setString(4, phone);
+            int i = pst.executeUpdate();
+            if (i>0){
+                JOptionPane.showMessageDialog(null, "Data saved successfully!!!");
+            }        
+            else {
+                JOptionPane.showMessageDialog(null, "Error Saving Data!!!");
+            }
+            
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
     }
     
     private void displayFromDatabase(){
