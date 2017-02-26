35524316a19add5084767cab2692015918c22a90
mainala <mainali_ayush@outlook.com>
2017-02-26 01:32:46 -0600

    add geocoding to locate address on map with marker

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ app.on('ready', () => {
     let win = new BrowserWindow({width:1000, height:800})
     win.setMenu(null);
     win.loadURL(`file://${__dirname}/index.html`)
+    win.webContents.openDevTools();
 })
