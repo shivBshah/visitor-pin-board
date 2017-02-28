e3ffd6048b713db383bca0092441865812a95dfd
Shiv Shah <shah.shivbhushan@gmail.com>
2017-02-28 12:35:19 -0600

    Create visitor form

---
+++ form/form-action.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    document.getElementById("next-btn").addEventListener('click', (event)=>{
+          document.getElementById("form").classList.toggle("show");
+    });
+    
+}
