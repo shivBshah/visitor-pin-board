4fb888224a1d10ad913fa2ba946457fc01ee78c3
Shiv Shah <shah.shivbhushan@gmail.com>
2017-02-28 20:34:58 -0600

    Modify form design

--- form/form-action.js
+++ form/form-action.js
@@ -1,8 +1,8 @@
 
-    document.getElementById("next-btn").addEventListener('click', (event)=>{
+  /*  document.getElementById("next-btn").addEventListener('click', (event)=>{
           document.getElementById("form").classList.toggle("show");
-    });
-    
+    });*/
+
     document.getElementById("vform").onsubmit = function saveDate(){
         var form = document.getElementById("vform");
         let values = [];
@@ -22,16 +22,33 @@
             } else {
                 if (e.name != "otherReason" && e.type != "submit"){
                     values.push(e.value);
-                }    
+                }
             }
         }
         console.log(values);
         alert(values);
         storeInDatabase(values);
     }
-    
-    function storeInDatabase(values){
-       
-        
-    }
 
+    function storeInDatabase(values){
+       alert("connecting to database");
+        var conn = dbConnection();
+
+        conn.connect((err)=>{
+          if(err){
+            alert('Database connection error');
+          } else{
+            alert('Database connection successful');
+          }
+        });
+        alert("connected to database");
+
+        conn.query(`INSERT INTO VISITOR(Fname, Lname, Email, HomeCity, HomeState
+        Zipcode, DestCity, DestState, TravelingFor, NumInParty, HearAboutCVB, Hotel)
+        VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`,values, (error, results, fields) => {
+          if (error) {
+            return alert("An error occurred with the query", error);
+          }
+          alert('Thank you for visiting Monroe West-Monroe Convention and Visitors Bureau');
+        });
+    }
