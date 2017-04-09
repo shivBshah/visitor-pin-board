c6976577d863ee5b2c8b9b738b38718c4bc7a13f
Shiv Shah <shah.shivbhushan@gmail.com>
2017-04-09 03:18:40 -0500

    Change File Structure

---
+++ app.js
@@ -0,0 +1,12 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+require('electron-reload')(__dirname);
+
+app.on('ready', () => {
+    let win = new BrowserWindow({show: false, kiosk:true})
+    win.setMenu(null);
+   win.maximize();
+    win.loadURL(`file://${__dirname}/main-view.html`)
+    win.show();
+  //  win.webContents.openDevTools();
+});
