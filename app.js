429e2de5a7f5bf1cd8767567697ae2954a423307
Shiv Shah <shah.shivbhushan@gmail.com>
2017-04-28 10:34:50 -0500

    change node settings

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
-
 require('electron-reload')(__dirname);
 
 app.on('ready', () => {
-    let win = new BrowserWindow({show: false})
+    let win = new BrowserWindow({show: false, kiosk:true})
     win.setMenu(null);
-    win.maximize();
-    win.loadURL(`file://${__dirname}/src/main.html`)
-
-    win.on('ready-to-show', function() {
-          win.show();
-          win.focus();
-      });
-    win.webContents.openDevTools();
-})
+   win.maximize();
+    win.loadURL(`file://${__dirname}/main-view.html`)
+    win.show();
+   win.webContents.openDevTools();
+});
